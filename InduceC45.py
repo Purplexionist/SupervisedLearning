297e3fa2afc4280232c0092e64b4981a0f4659ec
alexioso <alexbraksator@gmail.com>
2018-04-22 21:59:28 -0700

    Now transforms csv_numbers to a labeled numpy array of objects

--- InduceC45.py
+++ InduceC45.py
@@ -1,9 +1,11 @@
 import numpy as np
 import math
+import xml.etree.ElementTree as ET
+	
 
 def selectSplitting(attr, data, thresh, ratio):
 	dEntropy = findEntropy(data)
-	print(dEntropy)
+	#print(dEntropy)
 	dSize2 = data.shape[0]
 	gain = []
 	#iterate through each attribute
@@ -18,7 +20,7 @@ def selectSplitting(attr, data, thresh, ratio):
 			gain.append(curGain/attrEntropy)
 		else:
 			gain.append(curGain)
-		print(attrEntropy)
+		#print(attrEntropy)
 	bestIndex = gain.index(max(gain))
 	if(gain[bestIndex] > thresh):
 		return bestIndex
@@ -44,55 +46,109 @@ def findMostFrequent(data):
 	bestTuple = max(countEach.items(), key = lambda x: x[1])
 	return (bestTuple[0], bestTuple[1]/len(data))
 
-	
-def C45(test, attr, RootNode, classifiers):
+def indent(indent_counter):
+	return '	'*indent_counter
+
+
+def C45(test, attr, RootNode, classifiers, indent_counter, csv_number_labels):
 	if(len(np.unique(test[:,-1])) == 1):
-		print(classifiers[test[0,-1]])
+		#print(classifiers[test[0,-1]])
 		RootNode.leaf = Leaf(classifiers[test[0,-1]], test[0,-1], 1)
+		print(indent(indent_counter) + '<decision end = '+classifiers[test[0,-1]]+' choice ="'+test[0,-1]+'" p = "1.00"/>')
 	elif(len(attr) == 0):
 		freq = findMostFrequent(test)
-		print(classifiers[test[0,-1]])
+		#print(classifiers[test[0,-1]])
 		RootNode.leaf = Leaf(classifiers[freq[0]], freq[0], freq[1])
+		print(indent(indent_counter) + '<decision end = '+classifiers[freq[0]]+' choice ="'+freq[0]+'" p = "'+freq[1]+'"/>')
 	else:
-		splitNum = selectSplitting(attr, test, .1, 0)
+		splitNum = selectSplitting(attr, test, 1, 0)
 		if(splitNum == -1):
 			freq = findMostFrequent(test)
-			print(classifiers[test[0,-1]])
+			#print(classifiers[test[0,-1]])
 			RootNode.leaf = Leaf(classifiers[freq[0]], freq[0], freq[1])
 		else:
 			RootNode.attName = attr[splitNum]
-			print(RootNode.attName)
+			#print(RootNode.attName)
+			print(indent(indent_counter)+'<node var ="'+RootNode.attName+'">')
+			indent_counter += 1
 			for v in np.unique(test[: ,splitNum]):
-				print(v)
+				#print(v)
 				Dv = test[test[:, splitNum] == v]
 				Dv = np.delete(Dv, splitNum, axis = 1)
 				curAttr = np.delete(attr, splitNum)
 				tempNode = Node("")
-				C45(Dv, curAttr, tempNode, classifiers)
+				print(indent(indent_counter) + '<edge var ="'+v+'" num="'+find_num(csv_number_labels,v)+'">')
+				C45(Dv, curAttr, tempNode, classifiers, indent_counter+1, csv_number_labels)
 				newEdge = Edge(v, tempNode)
 				RootNode.edges.append(newEdge)
+				
+
+
+
+def read_csv_numbers(filepath):
+	file = open(filepath, 'r')
+	lines = file.readlines()
+	file.close()
+	attNames = lines[0].rstrip().split(",")[1:]
+	attLevels = list(map(int, lines[1].rstrip().split(",")[1:]))
+	classifier = lines[2].rstrip()
+
+	arr = np.empty((len(lines)-3,len(attLevels)))
+	for i in range(len(lines)-3):
+		arr[i] = lines[3+i].rstrip().split(",")[1:]
+
+	return(arr,attNames[0:-1])
+	
+
+def parse_xml(filepath, attr):
+	csv_number_labels = []
+	tree = ET.parse(filepath)
+	root = tree.getroot()
+	classifiers = {}
+	for child in root:
+		labels = [child.attrib['name']]
+		if(child.tag == "Category"):
+			for subcat in child:
+				classifiers[subcat.attrib['name']] = subcat.attrib['type']
+		for subchild in child:
+
+			labels.append(subchild.attrib['name'])
+		csv_number_labels.append(labels)
+	return csv_number_labels,classifiers
+
+def find_num(csv_number_labels, v):
+	i = 0
+	
+	for x in csv_number_labels:
+		j = 0
+		for y in x:
+
+			if y == v:
+				return str(j)
+			j+=1
+		i += 1
+
 
 def main():
-	test = np.array([[3,"N","T","S","N"],
-					 [3,"Y","T","S","Y"],
-					 [3,"Y","O","N","N"],
-					 [3,"Y","T","N","N"],
-					 [3,"N","O","N","N"],
-					 [3,"Y","T","S","Y"],
-					 [3,"Y","O","S","N"],
-					 [3,"N","T","S","N"],
-					 [4,"N","T","S","Y"],
-					 [4,"Y","O","N","N"],
-					 [4,"Y","O","S","Y"],
-					 [4,"N","T","N","N"],
-					 [4,"N","O","S","Y"],
-					 [4,"Y","O","S","Y"],
-					 [4,"N","T","N","N"],
-					 [4,"Y","O","N","N"]])
-	attr = np.array(["Bedrooms","Basement","Floorplan","Location"])
-	classifiers = {"N": 1, "Y": 2}
+	indent_counter = 0
+	print('<Tree name = "test">')
+	indent_counter += 1
+
+
+	test,attr = read_csv_numbers("tree02-20-numbers.csv")
+	csv_number_labels, classifiers = parse_xml("domain.xml", attr)
+
+
+	labeled_data = np.empty(test.shape, dtype = "object")
+	for col in range(test.shape[1]):
+		for row in range(test.shape[0]):
+			labeled_data[row,col] = csv_number_labels[col][int(test[row,col])]
+
 	RootNode = Node("")
-	C45(test, attr, RootNode, classifiers)
+
+	C45(labeled_data, attr, RootNode, classifiers, indent_counter,csv_number_labels)
+
+	
 
 class Leaf:
 	def __init__(self, decision, label, p):
