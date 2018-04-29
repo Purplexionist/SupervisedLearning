faea8816cf512f7733b257c774b13854542868e4
alexioso <alexbraksator@gmail.com>
2018-04-28 19:02:02 -0700

    restrictions

--- InduceC45.py
+++ InduceC45.py
@@ -242,18 +242,29 @@ def main():
 	if sys.argv[1] == "NULL":
 		labeled_data,attr,classifiers = read_iris(sys.argv[2])
 		csv_number_labels = None
+	#else, this program can read any categorical numbers csv file
 	else:
 		test,attr = read_csv_numbers(sys.argv[2])
 		csv_number_labels, classifiers = parse_xml(sys.argv[1], attr)
-	
-
 		labeled_data = np.empty(test.shape, dtype = "object")
 		for col in range(test.shape[1]):
 			for row in range(test.shape[0]):
 				labeled_data[row,col] = csv_number_labels[col][int(test[row,col])]
 	RootNode = Node("")
 	
+	if True:
+		restrictionsFile = open(sys.argv[5])
+		restrictions = restrictionsFile.readlines()[0].split(",")[1:]
+		print(len(restrictions))
+		for i in range(len(restrictions)-1,-1,-1):
+			print(i)
+			if restrictions[i] == '0':
+				labeled_data = np.delete(labeled_data,i,axis=1)
+				attr = np.delete(attr,i)
+			print(attr)
 
+	#except:
+	#	print("No restrictions file found/inputted")
 
 	#csv_number_labels is null for numeric
 	C45(labeled_data, attr, RootNode, classifiers, indent_counter,csv_number_labels)
