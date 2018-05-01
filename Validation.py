8e811bb55b842121ab44cf9a300b781d444c507f
alexioso <alexbraksator@gmail.com>
2018-04-30 17:43:33 -0700

    trees

--- Validation.py
+++ Validation.py
@@ -3,6 +3,8 @@ import math
 import xml.etree.ElementTree as ET
 import sys
 
+# py Validation.py data thresh ratio_flag n [restriction]
+
 def read_csv_numbers(filepath):
 	file = open(filepath, 'r')
 	lines = file.readlines()
@@ -17,6 +19,151 @@ def read_csv_numbers(filepath):
 		arr[i] = lines[3+i].rstrip().split(",")[1:]
 
 	return(arr,attNames[0:-1])
+
+def selectSplitting(attr, data, thresh, isNumeric):
+	dEntropy = findEntropy(data)
+	#print(dEntropy)
+	dSize2 = data.shape[0]
+	gain = []
+	#iterate through each attribute
+	if(isNumeric == 0):
+		for i in range(0,len(attr)):
+			attrEntropy = 0
+			gainEntropy = 0
+			#iterate through each value of an attribute
+			for uniqueValue in np.unique(data[:, i]):
+				dataWithValue = data[data[:,i] == uniqueValue]
+				attrEntropy += len(dataWithValue)/dSize2*findEntropy(dataWithValue)
+				gainEntropy += len(dataWithValue)/dSize2*math.log(len(dataWithValue)/dSize2, 2)
+			gainEntropy = -gainEntropy
+			if(gainEntropy == 0):
+				gainEntropy = 1
+			curGain = dEntropy - attrEntropy
+			if(sys.argv[4] == 1):
+				gain.append(curGain/gainEntropy)
+			else:
+				gain.append(curGain)
+			#print(curGain/gainEntropy)
+	else:
+		for i in range(0, len(attr)):
+			gainArray = []
+			for num in np.unique(data[:, i]):
+				attrEntropy = 0
+				gainEntropy = 0
+				dataUnder = data[data[:, i] <= num]
+				dataAbove = data[data[:, i] > num]
+				if(len(dataUnder) != 0):
+					attrEntropy += len(dataUnder)/dSize2*findEntropy(dataUnder)
+				if(len(dataAbove) != 0):
+					attrEntropy += len(dataAbove)/dSize2*findEntropy(dataAbove)
+				if(len(dataUnder) != 0):
+					gainEntropy += len(dataUnder)/dSize2*math.log(len(dataUnder)/dSize2, 2)
+				if(len(dataAbove) != 0):
+					gainEntropy += len(dataAbove)/dSize2*math.log(len(dataAbove)/dSize2, 2)
+				gainEntropy = -gainEntropy
+				if(gainEntropy == 0):
+					gainEntropy = 1
+				curGain = dEntropy - attrEntropy
+				if(sys.argv[4] == 1):
+					gainArray.append([num, curGain/gainEntropy])
+				else:
+					gainArray.append([num, curGain])
+			numMax = -999
+			alphaBest = -1
+			for inner in gainArray:
+				if(inner[1] > numMax):
+					numMax = inner[1]
+					alphaBest = inner[0]
+			gain.append([i, alphaBest, numMax])
+	if(isNumeric == 1):
+		curIndex = -1
+		curBestAlpha = -99
+		curBestNum = -99
+		for miniList in gain:
+			if(miniList[2] > curBestNum):
+				curBestNum = miniList[2]
+				curIndex = miniList[0]
+				curBestAlpha = miniList[1]
+		if(curBestNum > float(thresh)):
+			return curIndex, curBestAlpha
+		else:
+			return -1, 0
+	else:
+		bestIndex = gain.index(max(gain))
+		if(gain[bestIndex] > float(thresh)):
+			return bestIndex, 0
+		else:
+			return -1, 0
+
+def findEntropy(data):
+	uniqueClassifier = np.unique(data[:,-1])
+	entropy = 0
+	dSize = data.shape[0]
+	for c in uniqueClassifier:
+		isC = data[data[:,-1] == c]
+		lengthSplit = isC.shape[0]
+		entropy += (lengthSplit/dSize)*math.log(lengthSplit/dSize, 2)
+	return -entropy
+
+def findMostFrequent(data):
+	uniqueClassifiers = np.unique(data[:,-1])
+	countEach = {}
+	for item in uniqueClassifiers:
+		count = len(data[data[:,-1] == item])
+		countEach[item] = count
+	bestTuple = max(countEach.items(), key = lambda x: x[1])
+	return (bestTuple[0], bestTuple[1]/len(data))
+
+def C45(test, attr, RootNode, classifiers, isNumeric):
+
+	if(len(np.unique(test[:,-1])) == 1):
+		#print(classifiers[test[0,-1]])
+		RootNode.leaf = Leaf(classifiers[test[0,-1]], test[0,-1], 1)
+		
+	elif(len(attr) == 0):
+		freq = findMostFrequent(test)
+		#print(classifiers[test[0,-1]])
+		RootNode.leaf = Leaf(classifiers[freq[0]], freq[0], freq[1])
+		
+	else:
+		splitNum, alpha = selectSplitting(attr, test, sys.argv[2], isNumeric)
+		if(splitNum == -1):
+			freq = findMostFrequent(test)
+			RootNode.leaf = Leaf(classifiers[freq[0]], freq[0], freq[1])
+		else:
+			RootNode.attName = attr[splitNum]
+			
+			
+			if(isNumeric == 0):
+				for v in np.unique(test[: ,splitNum]):
+					#print(v)
+					Dv = test[test[:, splitNum] == v]
+					Dv = np.delete(Dv, splitNum, axis = 1)
+					curAttr = np.delete(attr, splitNum)
+					tempNode = Node("")
+					C45(Dv, curAttr, tempNode, classifiers,isNumeric)
+					newEdge = Edge(v, tempNode)
+					RootNode.edges.append(newEdge)
+			else:
+				Dv1 = test[test[:, splitNum] <= alpha]
+				#print(len(Dv1))
+				if(len(np.unique(Dv1[:, splitNum]) == 1)):
+					Dv1 = np.delete(Dv1, splitNum, axis = 1)
+					curAttr = np.delete(attr, splitNum)
+				tempNode = Node("")
+				C45(Dv1, curAttr, tempNode, classifiers,isNumeric)
+				newEdge = Edge(-alpha, tempNode)
+				RootNode.edges.append(newEdge)
+				Dv2 = test[test[:, splitNum] > alpha]
+				if(len(Dv2) != 0):
+					if(len(np.unique(Dv2[:, splitNum]) == 1)):
+						Dv2 = np.delete(Dv2, splitNum, axis = 1)
+						curAttr2 = np.delete(attr, splitNum)
+
+					tempNode2 = Node("")
+					C45(Dv2, curAttr2, tempNode2, classifiers,isNumeric)
+					newEdge2 = Edge(alpha, tempNode2)
+					RootNode.edges.append(newEdge2)
 
 def read_iris(filepath):
 	file = open(filepath,'r')
@@ -38,13 +185,12 @@ def read_iris(filepath):
 		arr[i] = [sl,sw,pl,pw,iris]
 	return arr,attNames,classifiers
 
-# py Validation.py data thresh ratio_flag n [restriction]
 def main():
-	indent_counter = 0
-	indent_counter += 1
-
+	
+	isNumeric = 0
 	#flag indicating this is numerical data; i.e iris dataset
-	if sys.argv[1] == "iris.data.txt":
+	if "iris" in sys.argv[1]:
+		isNumeric = 1
 		train,attr,classifiers = read_iris(sys.argv[1])
 	#else, this program can read any categorical numbers csv file
 	else:
@@ -91,10 +237,13 @@ def main():
 			testData.append(train[start : end])
 			curTrain = np.append(train[0 : start], train[end : n], 0)
 			RootNode = Node("")
+			C45(curTrain, attr, RootNode, classifiers,isNumeric)
+			trees.append(RootNode)
 
-
+	for tree in trees:
+		print(tree.attName)
 	#csv_number_labels is null for numeric
-	#C45(labeled_data, attr, RootNode, classifiers, indent_counter,csv_number_labels, f2)
+	
 
 class Leaf:
 	def __init__(self, decision, label, p):
